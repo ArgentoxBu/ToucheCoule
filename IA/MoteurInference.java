b03344bde880f0d715bebd705b3ceae75ad36b63
loictessier <loictessier.inge@gmail.com>
2015-02-24 21:40:04 -0500

    Ajout de la deserialisation du fichier de Regles dans MoteurInference.java

--- IA/MoteurInference.java
+++ IA/MoteurInference.java
@@ -1,5 +1,42 @@
 package IA;
 
-public class MoteurInference {
+import java.io.*;
+import java.util.*;
 
+public class MoteurInference {
+	
+	/************************Attributs*************************/
+	private final String rulesFilePath = "IA/regles/Battleship.rules";
+	private ArrayList<Regle> listeRegles;
+	
+	/**********************Constructeur************************/
+	public MoteurInference(){
+		ArrayList<String> data = deserialiserFichierRegle();
+		for(String s : data){
+			listeRegles.add(new Regle(s));
+		}
+	}
+	
+	/************************Methodes**************************/
+	private ArrayList<String> deserialiserFichierRegle(){
+		ArrayList<String> data = new ArrayList<String>();
+		Scanner scanner;
+		
+		try {
+			scanner = new Scanner(new File(rulesFilePath));
+			
+			while(scanner.hasNextLine()){
+				String line = scanner.nextLine();
+				data.add(line);
+			}
+			
+			scanner.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Fichier "+rulesFilePath+" non trouve : "+e.getStackTrace()[0]);
+		}		
+		
+		return data;
+	}
 }
