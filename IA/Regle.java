5fb74234ab6e9686808ef64531468f6ba38a00b2
ArgentoxBu <argento.bu@gmail.com>
2015-02-24 22:38:02 -0500

    AJout de satisfaitCondition et satisfaitConditions dans la classe Regle

--- IA/Regle.java
+++ IA/Regle.java
@@ -1,27 +1,49 @@
 package IA;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Regle {
-	private String[] premisses;
-	private String[] actions;
-	
+	private ArrayList<String> premisses;
+	private ArrayList<String> actions;
 	
 	public static void main(String[] args) {
-		System.out.println(new Regle("mode(Destruction),coupReussi,!bateau(coule)=>continuerDirection"));
+		Regle regle = new Regle("mode(Destruction),coupReussi,!bateau(coule)"
+				+ "=>continuerDirection");
+		System.out.println(regle);
+		System.out.println(regle.satisfaitCondition("coupReussi"));		// true
+		System.out.println(regle.satisfaitConditions(
+				"mode(Destruction)","coupReussi"));						// false
+		System.out.println(regle.satisfaitConditions(
+				"mode(Destruction)","coupReussi","!bateau(coule)"));	// true
 	}
 	
 	public Regle(String data) {		
 		String[] regleDecomposee = data.split("=>");
-		premisses = regleDecomposee[0].split(",");
-		actions = regleDecomposee[1].split(",");
+		premisses = new ArrayList<String>(Arrays.asList(
+				regleDecomposee[0].split(",")));
+		actions = new ArrayList<String>(Arrays.asList(
+				regleDecomposee[1].split(",")));
 	}
 	
-	/*public boolean satisfaitCondition(faits) {
-		
+	/**
+	 * Permet de savoir si un fait correspond à un prémisse d’une règle
+	 * @param fait
+	 * @return booléen
+	 */
+	public boolean satisfaitCondition(String fait) {
+		return premisses.contains(fait);
 	}
 	
-	public boolean satisfaitConditions(faits) {
-		
-	}*/
+	/**
+	 * Permet de savoir si un ensemble de faits satisfait tous les prémisses 
+	 * d’une règle
+	 * @param faits
+	 * @return booléen
+	 */
+	public boolean satisfaitConditions(String... faits) {
+		return Arrays.asList(faits).containsAll(premisses);
+	}
 
 	@Override
 	public String toString() {
